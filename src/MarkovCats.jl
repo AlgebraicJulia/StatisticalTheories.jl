ccdcfa2fe94def125f103e741e8eb13d010a7247
hhhults <hhhults@uw.edu>
2023-07-17 17:34:32 -0700

    got syntax tree for trivial example working

--- src/MarkovCats.jl
+++ src/MarkovCats.jl
@@ -1,7 +1,7 @@
 module MarkovCats
 using Catlab
-import Catlab.Theories:dom,codom
-export MarkovCats,FreeMarkovCategory,MarkovKernel,Stat,dom,codom
+import Catlab.Theories:dom,codom,otimes,id,compose,munit,mcopy,delete,braid,mmerge
+export MarkovCats,FreeMarkovCategory,MarkovKernel,Stat,dom,codom,otimes,id,compose,munit,mcopy,delete,braid,mmerge,Ob,Hom
 
 mutable struct MarkovKernel
    dom::Int
@@ -24,49 +24,55 @@ end
 So objects are finite dimensional vector spaces represented as ints and morphisms
 are 'Markov Kernels'
 """
-Stat = @instance ThMonoidalCategoryWithDiagonals{Int,MarkovKernel} begin
+@instance ThMonoidalCategoryWithBidiagonals{Int,MarkovKernel} begin
    dom(f::MarkovKernel) = f.dom
    codom(f::MarkovKernel) = f.codom
-   id(A::Int) = MarkovKernel(A,A,Expr(:call,Dirac))
+   id(A::Int) = MarkovKernel(A,A,Expr(:call,:Dirac))
    
    # question : do we want to support missing parameter values? turing does this...
    function compose(f::MarkovKernel,g::MarkovKernel)
       f.codom!=g.dom &&  error("domain mismatch between $f and $g")
-      out = []
       if isa(g.f,Expr)
          out = Expr(:call,g.f,isa(f.f,Expr) ? f.f : a for a in f.f )
       else
          isa(f.f,Expr) && error("domain mismatch: $f has one syntax tree, $g has more than one")
          it = 1
+         out = []
          for kernel in g.f
             domcounter = 0
             ex = []
             while domcounter<kernel.dom
                if domcounter + f.f[it].codom <= kernel.dom
-                  ex.append(f.f[it])
-                  it+=1
+                  push!(ex,f.f[it])
                   domcounter += f.f[it].codom
+                  it+=1
                else
                   error("domain mismatch or something... $f and $g don't compose well")
                end
             end
-            out.append(Expr(:call,kernel.f,ex...))
+            push!(out,MarkovKernel(kernel.dom,kernel.codom,Expr(:call,kernel.f,ex...)))
          end
+         out = map(x->convert(MarkovKernel,x),out)
       end
       return MarkovKernel(f.dom,g.codom,out)
    end
 
-   otimes(A::MarkovKernel,B::MarkovKernel) = MarkovKernel(A.dom+B.dom,A.codom+B.codom,[flatten(A);flatten(B)])
+   otimes(A::MarkovKernel,B::MarkovKernel) = MarkovKernel(A.dom+B.dom,A.codom+B.codom,[flatten!(A);flatten!(B)])
    otimes(A::Int,B::Int) = A+B
 
    # stuff below here is sketch / dubious
    munit(::Type{Int}) = 0
-   mcopy(A::Int) = A+A
-   delete(A::Int) = 0
-   braid(A::Int,B::Int) = nothing
+   create(A::Int) = MarkovKernel(0,A,Expr(:call,()->A))
+   mcopy(A::Int) = MarkovKernel(A,2*A,[id(A),id(A)])
+   mmerge(A::Int) = MarkovKernel(2*A,A,Expr(:call,:+,A,A))
+   delete(A::Int) = MarkovKernel(A,0,Expr(:call,x->0))
+
+   # don't use this; it doesn't do anything... humans were not meant to braid syntax trees
+   braid(A::Int,B::Int) = MarkovKernel(A+B,A+B,id(otimes(A,B)))
 end
 
-@syntax FreeMarkovCategory{ObExpr,HomExpr} ThMonoidalCategoryWithDiagonals begin
+
+@syntax FreeMarkovCategory{ObExpr,HomExpr} ThMonoidalCategoryWithBidiagonals begin
   otimes(A::Ob, B::Ob) = associate_unit(new(A,B), munit)
   otimes(f::Hom, g::Hom) = associate(new(f,g))
   compose(f::Hom, g::Hom) = associate_unit(new(f,g; strict=true), id)
--- src/StatisticalModel.jl
+++ src/StatisticalModel.jl
@@ -1,14 +1,25 @@
 using Catlab, Catlab.Theories
 include("MarkovCats.jl")
+include("PlateDiagrams.jl")
 using .MarkovCats
+using .PlateDiagrams
 
 θ = MarkovCats.Ob(FreeMarkovCategory.Ob,:θ)
 X = MarkovCats.Ob(FreeMarkovCategory.Ob,:X)
-P = MarkovCats.Hom(:P,θ,X)
+# P = MarkovCats.Hom(:P,θ,X)
+P₀ = MarkovCats.Hom(:P0,θ,X)
 ThStat = Presentation(FreeMarkovCategory)
 add_generator!(ThStat,θ)
 add_generator!(ThStat,X)
-add_generator!(ThStat,P)
+add_generator!(ThStat,P₀)
+# add_equation!(ThStat,)
+
+P = expand(PlateDiagram(:P,to_wiring_diagram(P₀)),2,true)
+
+gens = Dict(θ => 1,X => 1, P₀ => MarkovKernel(1,1,Expr(:call,:Beta)))
+
+model = functor((Int,MarkovKernel),to_hom_expr(FreeMarkovCategory,P),generators=gens)
+
 
 # # I am guessing this doesn't work due to my lack of understanding
 ## of the julia module system
@@ -21,10 +32,6 @@ add_generator!(ThStat,P)
 # @present ThIIDCounts <: ThStat begin
 #    P₀::Hom(θ,X)
 # end
-
-# ThIIDCounts = Presentation(FreeMarkovCategory) <: ThStat
-
-
 
 
 # model = @finfunctor ThLinear Stat 
